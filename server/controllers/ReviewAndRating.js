08d0c7691ce0a616981bf70dadeea0cd6b3ccedd
noormdafsar <nooruddin.mdafsar.it25@heritageit.edu.in>
2024-11-08 20:22:00 +0530

    getAllRating code added

--- server/controllers/ReviewAndRating.js
+++ server/controllers/ReviewAndRating.js
@@ -97,7 +97,35 @@ const getAverageRating = async ( req, res ) => {
     }
 }
 
+const getAllRating = async ( req, res ) => {
+    try {
+        const allReviews = await ReviewAndRating.find({})
+        .sort({ rating: 'desc' })
+        .populate({
+            path: 'user',
+            select: 'firstName lastName email image',
+        })
+        .populate({
+            path: 'course',
+            select: 'courseName',
+        })
+        .exec();
+        return res.status(200).json({
+            success: true,
+            message: 'All reviews fetched successfully',
+            data: allReviews,
+        });
+    }
+    catch(error) {
+        return res.status(500).json({
+            success: false,
+            message: 'Error while fetching all the reviews',
+        });
+    }
+}
+
 module.exports = {
     createRating,
     getAverageRating,
+    getAllRating,
 }
