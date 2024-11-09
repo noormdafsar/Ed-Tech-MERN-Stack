66bb5d1cf98cd32d068efaef529deebeb6732e65
noormdafsar <nooruddin.mdafsar.it25@heritageit.edu.in>
2024-11-09 09:48:18 +0530

    error resolved

--- server/controllers/Category.js
+++ server/controllers/Category.js
@@ -48,6 +48,75 @@ const showAllCategory = async (req, res) => {
     }
 }
 
+
+const categoryPageDetails = async ( req, res ) => {
+    try {
+        // get category id
+        const { categoryId } = req.body;
+        // get courses for specified category id
+        const selectedCategory = await Category.findById(categoryId)
+        .populate({
+            path: 'courses',
+            match: { status: 'Published' },
+            populate: 'courseInstructor',
+            select: 'courseName courseDescription thumbnail status',
+        })
+        .exec();
+        // validation
+        if(!selectedCategory) {
+            return res.status(404).json({
+                success: false,
+                message: 'Category not found',
+            });
+        }
+        // get courses for different categories
+        const differentCategories = await Category.find({
+            _id: { $ne: categoryId },
+        })
+        .populate({
+            path: 'courses',
+            match: { status: 'Published' },
+            populate: 'courseInstructor',
+            select: 'courseName courseDescription thumbnail status',
+        })
+        .exec();
+        // prepare the response
+        const categories = await Category.find({})
+        .populate({
+            path: 'courses',
+            match: { status: 'Published' },
+            populate: 'courseInstructor',
+            select: 'courseName courseDescription thumbnail status',
+        })
+        .exec();
+        const allCategories = await Category.find({})
+        .populate({
+            path: 'courses',
+            match: { status: 'Published' },
+            populate: 'courseInstructor',
+            select: 'courseName courseDescription thumbnail status',
+        })
+        .exec();
+        // return response
+        return res.status(200).json({
+            success: true,
+            message: 'Category page details fetched successfully',
+            data: {
+                selectedCategory,
+                differentCategories,
+                allCategories,
+            },  
+        });
+        
+    }
+    catch(error) {
+        return res.status(500).json({
+            success: false,
+            message: 'Error while fetching the category page details',
+        });
+    }
+}
+
 module.exports = {
     createCategory,
     showAllCategory,
