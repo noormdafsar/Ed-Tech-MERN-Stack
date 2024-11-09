20bb40ec5f37ad4cee03069f18a1b24466cc61f6
noormdafsar <nooruddin.mdafsar.it25@heritageit.edu.in>
2024-11-09 13:07:45 +0530

    profile router updated

--- server/routes/Profile.js
+++ server/routes/Profile.js
@@ -1,5 +1,12 @@
 const express = require('express');
-const Router = express.Router();
+const router = express.Router();
 const { auth, isInstructor, isAdmin, isStudent } = require('../middlewares/auth');
 
 const { upateProfile, deleteAccount } = require('../controllers/Profile');
+
+// routes
+router.put('/updateProfile', auth, upateProfile);
+router.delete('/deleteProfile', auth, deleteAccount);
+
+
+module.exports = router;
