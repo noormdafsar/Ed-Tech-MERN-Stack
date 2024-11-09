fdd0f212e3778a239929be55d6deeedf9a5acd0b
noormdafsar <nooruddin.mdafsar.it25@heritageit.edu.in>
2024-11-09 21:25:33 +0530

    more tailwind code added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,7 +11,8 @@ const Home = () => {
             <Link to={'/signup'}>
                 <div className='mt-16 p-1 mx-auto rounded-full bg-richblue-800 font-bold 
                 text-richblack-200 transition-all duration-200 hover:scale-95 w-fit'>
-                    <div className='flex items-center p-4'>
+                    <div className='flex items-center p-4 gap-2 rounded-full px-10 py-[5px]
+                    transition-all duration-200 group-hover:bg-richblack-900'>
                         <p>Become an Instructor</p>
                         <BiRightArrowAlt />
                     </div>
