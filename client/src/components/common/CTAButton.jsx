229dce46d97b603c94df626b96d44a0066cad89f
noormdafsar <nooruddin.mdafsar.it25@heritageit.edu.in>
2024-11-10 23:09:02 +0530

    few changes in home page and Navbar added

---
+++ client/src/components/common/CTAButton.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const CTAButton = ({linkto}) => {
+  return (
+    <div>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default CTAButton
